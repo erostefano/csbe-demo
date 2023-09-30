6db08337b0659dc468d240783bf43220b9975462
Stefano <st.mavilio@gmail.com>
2023-09-30 13:39:46 +0200

    add service

---
+++ src/main/java/com/example/demo/Person/PersonService.java
@@ -0,0 +1,10 @@
+package com.example.demo.Person;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+    public String getPerson() {
+        return "Stefano";
+    }
+}

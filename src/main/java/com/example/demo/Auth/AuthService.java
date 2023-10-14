ca2db10548649ef252fe216692074d7f610e6fab
Stefano <st.mavilio@gmail.com>
2023-10-14 08:47:55 +0200

    encode password

--- src/main/java/com/example/demo/Auth/AuthService.java
+++ src/main/java/com/example/demo/Auth/AuthService.java
@@ -3,6 +3,7 @@ package com.example.demo.Auth;
 import com.example.demo.Person.Person;
 import com.example.demo.Person.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +12,14 @@ public class AuthService {
     @Autowired
     private PersonRepository personRepository;
 
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
     public void register(RegistrationDto registrationDto) {
         Person person = new Person();
         person.setFirstName(registrationDto.getFirstName());
         person.setLastName(registrationDto.getLastName());
         person.setUserName(registrationDto.getUserName());
-        person.setPassword(registrationDto.getPassword());
+        person.setPassword(bCryptPasswordEncoder.encode(registrationDto.getPassword()));
         personRepository.save(person);
     }
 }

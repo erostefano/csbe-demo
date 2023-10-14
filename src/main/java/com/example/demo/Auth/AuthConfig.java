80b158400ed9c222a16256122ac1532caf58b336
Stefano <st.mavilio@gmail.com>
2023-10-14 11:04:47 +0200

    add basic auth

--- src/main/java/com/example/demo/Auth/AuthConfig.java
+++ src/main/java/com/example/demo/Auth/AuthConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.Auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,7 +13,10 @@ public class AuthConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
             authorizationManagerRequestMatcherRegistry.requestMatchers("/auth/registration").permitAll();
+            authorizationManagerRequestMatcherRegistry.anyRequest().authenticated();
         });
+
+        httpSecurity.httpBasic(Customizer.withDefaults());
 
         return httpSecurity
                 .csrf().disable().build();

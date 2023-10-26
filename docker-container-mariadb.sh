51f692cbe7e7204bb1b8e57607ad5e62a775783f
Stefano <st.mavilio@gmail.com>
2023-10-26 19:23:58 +0200

    update docker script

--- docker-container-mariadb.sh
+++ docker-container-mariadb.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Set your desired username and password
-DB_USER="admin"
-DB_PASS="admin"
-
-# Run the MariaDB container with the specified credentials
-sudo docker run -d --name my-mariadb-container -e MYSQL_ROOT_PASSWORD=my-secret-pw -p 3306:3306 mariadb
-
-echo "MariaDB container is running with username: $DB_USER and password: $DB_PASS"
+# username is root
+sudo docker run --name mariadb -e MYSQL_ROOT_PASSWORD=mypass -p 3306:3306 -d docker.io/library/mariadb:10.3
